9a38586fec2b609cf13ad34c166a0e1f04364e16
ravindrakasthala <61531637+ravindrakasthala@users.noreply.github.com>
2020-03-12 12:46:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+        console.log("hello");
+    } else {
+        document.getElementById("navbar").style.top = "-80px";
+
+        console.log("aaaa");
+    }
+    prevScrollpos = currentScrollPos;
+}
+
 postToGoogle();
 
 function postToGoogle() {
